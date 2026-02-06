3f753a103c920026879e0477c7257377236c24c2
lemteay <124504469+lemteay@users.noreply.github.com>
2026-02-06 13:52:26 +0800

    Take 2 add diff between static eval and eval as tt bonus
    bench 2800657

--- src/tt.cpp
+++ src/tt.cpp
@@ -97,8 +97,16 @@ void TTEntry::save(
     if (m || uint16_t(k) != key16)
         move16 = m;
 
+    int complexityBonus = 0;
+    if (value16 != VALUE_NONE && eval16 != VALUE_NONE)
+    {
+        complexityBonus = (std::abs(value16 - eval16) + 128) / 512;
+        complexityBonus = std::min(complexityBonus, 4);
+    }
+
     // Overwrite less valuable entries (cheapest checks first)
-    if (b == BOUND_EXACT || uint16_t(k) != key16 || d - DEPTH_ENTRY_OFFSET + 2 * pv > depth8 - 4
+    if (b == BOUND_EXACT || uint16_t(k) != key16
+        || d - DEPTH_ENTRY_OFFSET + 2 * pv > depth8 - 4 + complexityBonus
         || relative_age(generation8))
     {
         assert(d > DEPTH_ENTRY_OFFSET);

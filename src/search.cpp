4d5df925ca97b357396d019d31f84faf3f9a9bb5
lemteay <polylemtea@gmail.com>
2026-03-09 12:47:37 +0800

    2

--- src/search.cpp
+++ src/search.cpp
@@ -1257,7 +1257,7 @@ moves_loop:  // When in check, search starts here
 
                 // Post LMR continuation history updates
                 update_continuation_histories(ss, movedPiece, move.to_sq(),
-                                              1342 + 150 * (priorReduction >= 3));
+                                              1342 + 30 * priorReduction);
             }
         }
 

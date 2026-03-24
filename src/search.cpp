5a9a022accce752959d2849d064e5a48b8dd8817
lemteay <polylemtea@gmail.com>
2026-03-24 21:38:22 +0800

    bench 2634235

--- src/search.cpp
+++ src/search.cpp
@@ -1267,7 +1267,7 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = d < newDepth && value > bestValue + 48;
+                const bool doDeeperSearch    = d < newDepth && value > bestValue + 51 - ss->ttPv * 29;
                 const bool doShallowerSearch = value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;

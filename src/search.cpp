55e844fa38b77edad0fcbd57e60dd30ad285ba16
lemteay <polylemtea@gmail.com>
2026-05-12 19:56:02 +0800

    bench 2436278

--- src/search.cpp
+++ src/search.cpp
@@ -1296,7 +1296,8 @@ moves_loop:  // When in check, search starts here
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates
-                update_continuation_histories(ss, movedPiece, move.to_sq(), 1415);
+                if (value > alpha)
+                    update_continuation_histories(ss, movedPiece, move.to_sq(), 1415);
             }
         }
 
